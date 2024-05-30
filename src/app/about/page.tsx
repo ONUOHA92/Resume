0249f638518838a5244353837e798f9409d30416
Onuoha Collins <onuohacollinschidubem@gmail.com>
2024-05-30 11:52:34 +0100

    added my about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -10,7 +10,7 @@ import {
   LinkedInIcon,
   XIcon,
 } from '@/components/SocialIcons'
-import portraitImage from '@/images/portrait.jpg'
+import portraitImage from '@/images/avatar2.jpeg'
 
 function SocialLink({
   className,
@@ -50,7 +50,7 @@ function MailIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 export const metadata: Metadata = {
   title: 'About',
   description:
-    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+    'I’m Onuoha Collins. I live in Lagos Nigeria, where I design the future.',
 }
 
 export default function About() {
@@ -69,35 +69,36 @@ export default function About() {
         </div>
         <div className="lg:order-first lg:row-span-2">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            I’m Spencer Sharp. I live in New York City, where I design the
+            I’m Onuoha Collins. I live in Lagos Nigeria, where I design the
             future.
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
-              I’ve loved making things for as long as I can remember, and wrote
-              my first program when I was 6 years old, just two weeks after my
-              mom brought home the brand new Macintosh LC 550 that I taught
-              myself to type on.
+              I’ve loved making things for as long as I can remember, and
+              started my first coding career in 2018 which is self thought, when
+              I was working as a Sales executive, so i had to save up to buy my
+              first laptop for coding. .
             </p>
             <p>
-              The only thing I loved more than computers as a kid was space.
-              When I was 8, I climbed the 40-foot oak tree at the back of our
-              yard while wearing my older sister’s motorcycle helmet, counted
-              down from three, and jumped — hoping the tree was tall enough that
-              with just a bit of momentum I’d be able to get to orbit.
+              I also teach children coding using JavaScript, Python, and Scratch
+              programming, helping them develop essential skills and a passion
+              for technology from a young age. Through interactive lessons and
+              hands-on projects, I aim to inspire creativity, foster
+              problem-solving abilities, and equip them with the tools they need
+              to succeed in the digital world
             </p>
             <p>
-              I spent the next few summers indoors working on a rocket design,
-              while I recovered from the multiple surgeries it took to fix my
-              badly broken legs. It took nine iterations, but when I was 15 I
-              sent my dad’s Blackberry into orbit and was able to transmit a
-              photo back down to our family computer from space.
+              I spent most of my time working on client projects and learning
+              new things, which significantly enhanced my skills and broadened
+              my expertise in the field. This dedication to continuous
+              improvement allowed me to develop innovative solutions, stay
+              updated with the latest industry trends, and deliver high-quality
+              results that consistently exceeded client expectations.
             </p>
             <p>
-              Today, I’m the founder of Planetaria, where we’re working on
-              civilian space suits and manned shuttle kits you can assemble at
-              home so that the next generation of kids really <em>can</em> make
-              it to orbit — from the comfort of their own backyards.
+              Today, I’m the founder of ExcrowPro, where we’re working on Escrow
+              app that will be bridging the gap of fruad between buyers and
+              seller in Nigeria.
             </p>
           </div>
         </div>
@@ -106,21 +107,33 @@ export default function About() {
             <SocialLink href="#" icon={XIcon}>
               Follow on X
             </SocialLink>
-            <SocialLink href="#" icon={InstagramIcon} className="mt-4">
+            <SocialLink
+              href="https://www.instagram.com/onuoha_dev/"
+              icon={InstagramIcon}
+              className="mt-4"
+            >
               Follow on Instagram
             </SocialLink>
-            <SocialLink href="#" icon={GitHubIcon} className="mt-4">
+            <SocialLink
+              href="https://github.com/ONUOHA92"
+              icon={GitHubIcon}
+              className="mt-4"
+            >
               Follow on GitHub
             </SocialLink>
-            <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+            <SocialLink
+              href="https://www.linkedin.com/in/collins-chidubem-onuoha-2a7b19155/"
+              icon={LinkedInIcon}
+              className="mt-4"
+            >
               Follow on LinkedIn
             </SocialLink>
             <SocialLink
-              href="mailto:spencer@planetaria.tech"
+              href="mailto:onuohacollinschidubem@gmail.com"
               icon={MailIcon}
               className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
             >
-              spencer@planetaria.tech
+              onuohacollinschidubem@gmail.com
             </SocialLink>
           </ul>
         </div>
